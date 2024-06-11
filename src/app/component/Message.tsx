24584e1b67dfd3e4048182f33d0c95ae03c7ed6a
cnann0615 <leave.it.to.7010@gmail.com>
2024-06-11 23:40:19 +0900

    0610トップページの構成を変え、Profileへの遷移ボタンを押下可能にした。

--- src/app/component/Message.tsx
+++ src/app/component/Message.tsx
@@ -9,13 +9,37 @@ function Message() {
   return (
     <div className=" text-gray-100 font-bold mt-10 mb-16 md:mb-36 ">
       <Animation animation="fadeInUp" rootMargin="-200px">
-        <h1 className=" mt-12 mb-20 xl:mt-28 xl:mb-40 text-center text-lg sm:text-4xl lg:text-5xl xl:text-7xl">
+        <h1 className=" -mb-[120px] xl:-mb-[30px] mt-16 sm:mt-28 text-center text-lg sm:text-4xl lg:text-5xl xl:text-7xl ">
           ”人はいつからでも、何にでもなれる。”
         </h1>
       </Animation>
+      <div className="mb-12 xl:mb-24 z-0">
+        <div className=" relative ">
+          <div className=" md:truncate">
+            <Earth />
+          </div>
+          <div className=" text-center xl:text-left absolute  top-[35%] lg:top-[40%] xl:top-[34%] left-0 xl:ml-24">
+            <Animation animation="fadeInUp" rootMargin="-200px">
+              <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl xl:leading-relaxed">
+                新卒入社後
+                <span className="text-yellow-500">9ヶ月で退職</span>し
+                <br />
+                <span className="text-yellow-500">カナダ🇨🇦</span>に出発。
+                <br />
+                半年間、日本を離れ、
+                <br />
+                国を超え、
+                <span className="text-yellow-500">ラクロスの本場</span>
+                である<span className="text-yellow-500">カナダ</span>
+                での修行を決意。
+              </h1>
+            </Animation>
+          </div>
+        </div>
+      </div>
       <div>
         <Animation animation="fadeInUp" rootMargin="-300px">
-          <div className=" xl:flex -mb-[100px] xl:-mb-[200px] z-10">
+          <div className=" xl:flex  -mt-[140px]  xl:-mt-[180px] z-10">
             <div className=" mb-20 lg:mb-36 w-full xl:w-1/2 flex items-center justify-center ">
               <div className=" md:hidden relative">
                 <Link href={"/gallery"} className="">
@@ -66,7 +90,7 @@ function Message() {
               </div>
             </div>
 
-            <div className="w-full xl:w-1/2 flex items-center">
+            <div className="mb-20 xl:mb-0 w-full xl:w-1/2 flex items-center">
               <div className=" mx-auto xl:mx-0 text-center xl:text-left">
                 <h1 className=" mb-4 text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl xl:leading-relaxed">
                   2028年
@@ -86,32 +110,8 @@ function Message() {
             </div>
           </div>
         </Animation>
-        <div className="mb-12 xl:mb-24 z-0">
-          <div className=" relative ">
-            <div className=" md:truncate">
-              <Earth />
-            </div>
-            <div className=" text-center xl:text-left absolute  top-[35%] lg:top-[40%] xl:top-[34%] left-0">
-              <Animation animation="fadeInUp" rootMargin="-200px">
-                <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl xl:leading-relaxed">
-                  新卒入社後
-                  <span className="text-yellow-500">9ヶ月で退職</span>し
-                  <br />
-                  <span className="text-yellow-500">カナダ🇨🇦</span>に出発。
-                  <br />
-                  半年間、日本を離れ、
-                  <br />
-                  国を超え、
-                  <span className="text-yellow-500">ラクロスの本場</span>
-                  である<span className="text-yellow-500">カナダ</span>
-                  での修行を決意。
-                </h1>
-              </Animation>
-            </div>
-          </div>
-        </div>
 
-        <div className=" xl:flex -mt-[150px]  xl:-mt-[100px]">
+        <div className=" xl:flex">
           <div className="w-full xl:w-1/2">
             <Animation animation="zoomIn" rootMargin="-300px">
               <Image
